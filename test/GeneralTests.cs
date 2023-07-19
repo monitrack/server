601c46e47b85ad0bb3e473fd8d790206afcb7094
Shekhov Ismoil <veyxov@gmail.com>
2023-07-19 10:44:36 +0500

    test: added first creation test

--- test/GeneralTests.cs
+++ test/GeneralTests.cs
@@ -1,10 +1,27 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Server.Controllers;
+
 namespace test;
 
 public class ExpenseTests
+    : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void CreateExpenseTests()
-    {
+    private readonly WebApplicationFactory<Program> _factory;
 
+    public ExpenseTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task CreateExpenseTestsAsync()
+    {
+        var client = _factory.CreateClient();
+
+        var content = new ExpenseCreateDto(100.0M);
+        var response = await client.PostAsJsonAsync("/expense", content);
+
+        response.EnsureSuccessStatusCode();
     }
 }
